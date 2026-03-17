dd91f052d53f9ebfec86bf26f581e4b03347db57
AbdulRahman-Habbal <ABDH28560@gmail.com>
2026-03-17 19:06:52 +0200

    Update security.tf

--- security.tf
+++ security.tf
@@ -99,3 +99,11 @@ resource "aws_security_group" "backend_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+resource "aws_security_group_rule" "allow_alb_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"] # Allows the world to see your ALB
+  security_group_id = aws_security_group.alb_sg.id
+}
